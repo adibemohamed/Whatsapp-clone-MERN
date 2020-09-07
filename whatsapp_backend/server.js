8e7a2673958a6b8f867f963d12a72dc80546fce5
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-07 23:46:34 +0000

    tst

---
+++ whatsapp_backend/server.js
@@ -0,0 +1,17 @@
+// importing
+import express from 'express';
+
+
+// app config 
+const app = express();
+
+//middlaware
+
+// database config
+
+// ????
+
+// api routes 
+app.get('/', (req, res) => res.status(200).send('hellow world'))
+
+// listen
