7d0716422cb7ff2f764d70e15a9a1e7fbf797e19
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-09 18:16:01 +0000

    create message in db

--- whatsapp_backend/server.js
+++ whatsapp_backend/server.js
@@ -21,7 +21,20 @@ mongoose.connect(connection_url, {
 // ????
 
 // api routes 
-app.get('/', (req, res) => res.status(200).send('hello world'))
+app.get('/', (req, res) => res.status(200).send('hello world'));
+
+app.post('/api/v1/messages/new', (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.status(201).send(`new message created: \n ${data}`);
+        }
+    })
+})
 
 // listen
 app.listen(port, () => console.log(`Listen on localhost:${port}`));
