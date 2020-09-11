2bffa3b29f11dc2fa5371c9e3b5245ea1ed57a42
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-11 00:35:12 +0000

    ADD pusher to fron-end

--- whatsapp_frontend/src/App.js
+++ whatsapp_frontend/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import './App.css';
-import Chat from './Chat';
-import { Sidebar } from './Sidebar'; 
+import React from "react";
+import "./App.css";
+import Chat from "./Chat";
+import { Sidebar } from "./Sidebar";
 
 function App() {
+  useEffect(() => {
+    var pusher = new Pusher("e78d980c8ec2309ce18c", {
+      cluster: "eu",
+    });
+    var channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
-
         <Sidebar />
 
         <Chat />
-
       </div>
     </div>
   );
