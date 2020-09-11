d7e351e83982f18aebf7045e7ae44b3ffeb8b6a4
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-11 00:39:02 +0000

    Real database connection

--- whatsapp_frontend/src/App.js
+++ whatsapp_frontend/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import { Sidebar } from "./Sidebar";
+import Pusher from "pusher-js";
 
 function App() {
   useEffect(() => {
