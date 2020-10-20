18fe35bfc8ac692736f6e251e1012b1b394bd876
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-10-20 16:30:05 +0000

    rm pr

--- whatsapp_frontend/src/firebase.js
+++ whatsapp_frontend/src/firebase.js
@@ -1,19 +1,11 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCoq3jtgW2f-Kp0uw4NW6unyjO3mOjpUDE",
-    authDomain: "whatsapp-clone-9888b.firebaseapp.com",
-    databaseURL: "https://whatsapp-clone-9888b.firebaseio.com",
-    projectId: "whatsapp-clone-9888b",
-    storageBucket: "whatsapp-clone-9888b.appspot.com",
-    messagingSenderId: "801234079623",
-    appId: "1:801234079623:web:43aa25abafef9524e54d22",
-    measurementId: "G-72Z37S51YP"
-  };
+  // put your firebase config here
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  const db = firebaseApp.database();
+const db = firebaseApp.database();
 
-
-  export default {db}
+export default { db };
